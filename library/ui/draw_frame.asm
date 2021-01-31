f4f0a8656148c43f6217317ada8f80f975ecbf11
Tim Soderstrom <tim@moocowproductions.org>
2021-01-30 18:23:24 -0600

    Still working on basic UI frame stuff

--- library/ui/draw_frame.asm
+++ library/ui/draw_frame.asm
@@ -1,13 +1,50 @@
 .proc draw_frame
+  DISPLAY_END_X = $4F
+  DISPLAY_END_Y = $3B
 draw_frame:
+  ; color
+  lda #FRAME_COLOR
+  sta r0
 
-  ; Draw Frame
-  ; Note for files, seems like the first 4 bytes of the file are skipped
-  ;lda #<frame_filename
-  ;sta r0
-  ;lda #>frame_filename
-  ;sta r0+1
-  ;lda #$09  ; file length
-  ;jsr files::load
-  ;jsr load_screen
+  ;x/y start
+  lda #$0
+  sta r1
+  sta r3
+  ; x end
+  lda #DISPLAY_END_X
+  sta r2
+  ; y end
+  lda #DISPLAY_END_Y
+  sta r4
+
+  jsr graphics::drawing::draw_rounded_box
+  lda #$00
+  tax
+  jsr graphics::drawing::goto_xy
+
+@color_fill_frame:
+  ; X/Y Start
+  lda #$01
+  sta r0
+  sta r1
+  ; X End
+  lda #$4E
+  sta r2
+  ; Y End
+  lda #$3A
+  sta r3
+  lda #HEADER_BACKGROUND_COLOR
+  sta r4
+  jsr graphics::drawing::draw_solid_box
+
+@print_top_frame_titles:
+  print_string_macro command_tracker_string, #PROGRAM_TITLE_X, #PROGRAM_TITLE_Y, #TITLE_COLORS
+  print_string_macro song_label, #SONG_TITLE_LABEL_X, #SONG_TITLE_LABEL_Y, #TITLE_COLORS
+  print_string_macro composer_label, #COMPOSER_LABEL_X, #COMPOSER_LABEL_Y, #TITLE_COLORS
+  print_string_macro speed_label, #SPEED_LABEL_X, #SPEED_LABEL_Y, #TITLE_COLORS
+  print_string_macro pattern_label, #PATTERN_LABEL_X, #PATTERN_LABEL_Y, #TITLE_COLORS
+  print_string_macro order_label, #ORDER_LABEL_X, #ORDER_LABEL_Y, #TITLE_COLORS
+  print_string_macro row_label, #ROW_LABEL_X, #ROW_LABEL_Y, #TITLE_COLORS
+
+  rts
 .endproc
--- ui.asm
+++ ui.asm
@@ -1,6 +1,16 @@
 ; Graphic routines
 
 .scope ui
+  ; data
+  command_tracker_string: .byte "command tracker v0.000",0
+  song_label: .byte "song:",0
+  composer_label: .byte "by  :",0
+  speed_label: .byte "spd:",0
+  pattern_label: .byte "pat:",0
+  order_label: .byte "ord:  /",0
+  row_label: .byte "row:",0
+
+  ; includes
   .include "library/ui/draw_frame.asm"
   .include "library/ui/print_song_info.asm"
   .include "library/ui/print_current_pattern_number.asm"
@@ -11,6 +21,7 @@
   .include "library/ui/print_pattern.asm"
   .include "library/ui/print_note.asm"
   .include "library/ui/scroll_pattern.asm"
+
 
 
 .endscope
