806e3ed558607d7c9ec2ce6368ff3a8e0fcfda71
Tim Soderstrom <tim@moocowproductions.org>
2021-02-10 22:24:21 -0600

    Pattern now pans to the right to the max channel

--- library/tracker/modules/edit_pattern.asm
+++ library/tracker/modules/edit_pattern.asm
@@ -8,7 +8,12 @@ VOLULME       = $02
 EFFECT        = $03
 EFFECT_VALUE  = $04
 SCREEN_ROW_MAX = $30
+NUM_CHANNELS_VISIBLE = $06 ; Number of channels to see at once
 
+CURSOR_X_START = $03  ; We need to skip past the row numbers and things
+; What cursor position to start when moving past last screen channel
+; This puts the cursor at the beginning of screen channel 6
+CURSOR_X_START_OF_LAST_CHNNAEL = $3F
 ; If col pos is 0, note
 ; If 3 inst
 ; If 5 vol
@@ -25,10 +30,10 @@ LAST_COLUMN_POSITION = $0A
 ; Vars
 COLOR           = r0
 ; Screen row pos (may differ from pattern when scrolling)
-SCREEN_ROW      = r5
-SCREEN_CHANNEL  = r5 + 1
 COLUMN_POS      = r6       ; Note, Inst, Vol, Eff, Effect Value
 PITCH_TEMP      = r7
+SCREEN_ROW      = r9
+SCREEN_CHANNEL  = r9 + 1
 
 ; Initial run once stuff
 edit_pattern:
@@ -42,11 +47,11 @@ edit_pattern:
   stz CHANNEL_NUMBER
   stz SCREEN_CHANNEL
   stz COLUMN_POS
+  stz ROW_NUMBER
   jsr ui::print_current_pattern_number
   jsr ui::print_row_number
 
-
-  lda #$03
+  lda #CURSOR_X_START
   sta cursor_x
   lda #$FF
   sta VERA_L0_hscroll_h
@@ -193,8 +198,7 @@ edit_pattern_loop:
   jsr ui::cursor_left
   jsr ui::cursor_left
   jsr ui::cursor_left
-  lda #$00
-  sta COLUMN_POS
+  stz COLUMN_POS
   jmp edit_pattern_loop
 @cursor_left_channel:
   lda CHANNEL_NUMBER
@@ -224,12 +228,36 @@ edit_pattern_loop:
   sta COLUMN_POS
   jmp edit_pattern_loop
 @cursor_right_channel:
+  ; If we're on the last column, we need to move the pattern view over
+  lda SCREEN_CHANNEL
+  cmp #NUM_CHANNELS_VISIBLE - 1
+  bne @cursor_move_right_channel
+
+  ; Check if we're not on the max possible channel
+  lda START_CHANNEL
+  clc
+  adc #NUM_CHANNELS_VISIBLE
+  cmp #NUMBER_OF_CHANNELS
+  bne @not_at_last_channel
+  jmp edit_pattern_loop
+
+@not_at_last_channel:
+  inc START_CHANNEL
+  jsr ui::draw_pattern_frame
+  jsr ui::print_pattern
+
+  ; Move to beginning of shifted channel
+  lda #CURSOR_X_START_OF_LAST_CHNNAEL
+  sta cursor_x
+  jsr graphics::drawing::cursor_plot
+  jmp @cursor_right_channel_end
+@cursor_move_right_channel:
   jsr ui::cursor_right
   jsr ui::cursor_right
-  lda #$00
-  sta COLUMN_POS
-  inc CHANNEL_NUMBER
   inc SCREEN_CHANNEL
+@cursor_right_channel_end:
+  stz COLUMN_POS
+  inc CHANNEL_NUMBER
   jmp edit_pattern_loop
 
 @cursor_tab_right:
@@ -487,7 +515,6 @@ reset_scroll_position:
   sta VERA_L0_vscroll_l
   stz ROW_NUMBER
   stz SCREEN_ROW
-
   lda #$01
   sta cursor_layer
   lda cursor_x
